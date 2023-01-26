e9e60187095ba2d883f315bac30ecd45e5570632
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-01-26 10:20:16 -0500

    add stuff

--- signout.py
+++ signout.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, session, g
+from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 
 import tools
@@ -14,19 +15,60 @@ def home():
 
 @signout.route("/register")
 def register():
-    return "register now"
+    return render_template("register.html")
 
-@signout.route("/new-registration", methods=["POST"])
-def new_admin():
-    return "new admin account"
+@signout.route("/register/handler", methods=["POST"])
+def register_handler():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    cur.execute(
+        "INSERT INTO users (username, password_hash, config) VALUES (?, ?, ?)",
+        (username, generate_password_hash(password), "{}")
+    )
+    con.commit()
+    
+    return redirect(url_for("signout.login", fillname=username, message=f"Account {username} successfully created. You can now login."))
+
+@signout.route("/login")
+def login():
+    message = request.args.get("message")
+    fillname = request.args.get("fillname")
+    return render_template("login.html", fillname=fillname, message=message)
+
+@signout.route("/login/handler", methods=["POST"])
+def login_hander():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user = cur.execute(
+        "SELECT * FROM user WHERE username = ?", (username,)
+    ).fetchone()
+
+    if user:
+        if check_password_hash(user["password_hash"], password):
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("signout.panel"))
+
+        else:
+            return redirect(url_for("signout.login", message="Incorrect password!"))
+
+    else:
+        return redirect(url_for("signout.login", message="That account doesn't exist!"))
 
 @signout.route("/panel")
 def panel():
-    return "signed in panel"
+    message = request.args.get("message")
+    return render_template("panel.html", message=message)
 
 @signout.route("/panel/settings")
 def settings():
     return "settings page"
+
+@signout.route("/panel/settings/apply")
+def apply_settings():
+    return "apply settings"
 
 @signout.route("/panel/monitor")
 def monitor():
@@ -35,3 +77,7 @@ def monitor():
 @signout.route("/panel/student")
 def student():
     return "student panel"
+
+@signout.route("/share/<public_id>")
+def student_public():
+    return "student panel public share link"
--- signout.sql
+++ signout.sql
@@ -1,8 +1,7 @@
-CREATE TABLE logins (
+CREATE TABLE users (
     id INTEGER PRIMARY KEY NOT NULL,
-    login_email TEXT NOT NULL,
-    login_salt TEXT NOT NULL,
-    login_hash TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
     config TEXT NOT NULL
 );
 
