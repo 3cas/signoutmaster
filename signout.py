2b15146a5e50396b776196a96c4c762690a26bff
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-02-03 09:29:08 -0500

    work on signout system

--- signout.py
+++ signout.py
@@ -1,13 +1,48 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
+from datetime import datetime
 
 import tools
 
-con = sqlite3.connect(tools.inst("signout.db"))
+con = sqlite3.connect(tools.inst("signout.db"), check_same_thread=False)
 cur = con.cursor()
 
 signout = Blueprint("signout", __name__)
+
+ALLOWED_USERNAME = "abcdefghijklmnopqrstuvwxyz1234567890_-"
+
+def now(): return int(datetime.timestamp(datetime.now()))
+
+def check_user():
+    if ("user_id" not in session) or (not session["user_id"]):
+        return False
+    
+    user = cur.execute(
+        "SELECT username FROM users WHERE id = ?",
+        (session["user_id"],)
+    ).fetchone()
+
+    if not user:
+        session.clear()
+        return False
+
+    return True
+
+def no_user():
+    flash("error:You must be signed in!")
+    return redirect(url_for("signout.login"))
+
+# if not check_user(): return no_user()
+
+@signout.app_template_filter("split")
+def my_split(text: str, splitter: str):
+    return text.split(splitter)
+
+@signout.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, post-check=0, pre-check=0"
+    return response
 
 @signout.route("/")
 def home():
@@ -22,13 +57,17 @@ def register_handler():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    cur.execute(
-        "INSERT INTO users (username, password_hash, config) VALUES (?, ?, ?)",
-        (username, generate_password_hash(password), "{}")
-    )
-    con.commit()
+    try:
+        cur.execute(
+            "INSERT INTO users (username, password_hash, config) VALUES (?, ?, ?)",
+            (username, generate_password_hash(password), "{}")
+        )
+    except sqlite3.IntegrityError:
+        return "already exists"
+    else:
+        con.commit()
     
-    flash("Account {username} successfully created. You can now login.")
+    flash("success:Account {username} successfully created. You can now login.")
     return redirect(url_for("signout.login"))
 
 @signout.route("/login")
@@ -36,50 +75,74 @@ def login():
     return render_template("login.html")
 
 @signout.route("/login/handler", methods=["POST"])
-def login_hander():
+def login_handler():
     username = request.form.get("username")
     password = request.form.get("password")
 
     user = cur.execute(
-        "SELECT * FROM user WHERE username = ?", (username,)
+        "SELECT id, password_hash FROM users WHERE username = ?", (username,)
     ).fetchone()
 
     if user:
-        if check_password_hash(user["password_hash"], password):
+        if check_password_hash(user[1], password):
             session.clear()
-            session["user_id"] = user["id"]
+            session["user_id"] = user[0]
+            flash(f"success:Successfully signed in as {username}!")
             return redirect(url_for("signout.panel"))
 
-        flash("Incorrect password!")
+        else:
+            flash("error:Incorrect password!")
 
-    flash("That account doesn't exist!")
+    else:
+        flash("error:That account doesn't exist!")
 
     return redirect(url_for("signout.login"))
 
+@signout.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("signout.home"))
+
 @signout.route("/panel")
 def panel():
-    return render_template("panel.html")
+    if not check_user(): return no_user()
+
+    username = cur.execute(
+        "SELECT username FROM users WHERE id = ?",
+        (session["user_id"],)
+    ).fetchone()[0]
+
+    return render_template("panel.html", username=username)
 
 @signout.route("/panel/settings")
 def settings():
+    if not check_user(): return no_user()
+
     return render_template("settings.html")
 
 @signout.route("/panel/settings/apply")
 def apply_settings():
+    if not check_user(): return no_user()
     # ...
-    flash("Applied changes!")
+    flash("success:Applied changes!")
     return redirect(url_for("signout.settings"))
 
 @signout.route("/panel/monitor")
 def monitor():
+    if not check_user(): return no_user()
+
     return "monitoring page"
 
 @signout.route("/panel/student")
 def student():
+    if not check_user(): return no_user()
+
     return "student panel"
 
-@signout.route("/panel/sign")
+@signout.route("/panel/sign", methods=["POST"])
 def sign():
+    if not check_user(): return no_user()
+
     return "signing out or in"
 
 @signout.route("/share/<public_id>")
