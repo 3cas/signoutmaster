3ee8a9d25023de0988b41fed2ea53776cfdb975f
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-06-02 09:46:17 -0400

    fix tools

--- tools.py
+++ tools.py
@@ -1,5 +1,4 @@
 from flask import g, Blueprint, url_for, render_template, request
-import requests
 import os
 import sqlite3
 import json
@@ -27,9 +26,6 @@ def inst(*targets):
 
 def util(*targets):
     return path("utility", *targets)
-
-with open(util("secrets.json"), "r") as f:
-    SECRETS = json.load(f)
 
 try:
     with open(inst("id.txt"), "r") as f:
@@ -109,37 +105,4 @@ class MyBlueprint(Blueprint):
     def render_error(self, code, message):
         return self.render("error.html", code=code, message=message), code
 
-def debug(content):
-    requests.post(
-        "https://discord.com/api/webhooks/1041385180117618708/6QV6Yc1ZgpUxoD-VcgFwQCn7kYjH5Aaf8JEpKXFWHNph8wEsjkCSlKXXRkxjis4yvFAA",
-        json = {
-            "content": content
-        }
-    )
-
-def send_email(recipients: list, subject: str, body: str, category: str, sender_name: str = "weirdcease.com", sender_address: str = "no-reply@weirdcease.com"):
-    payload = {
-        "from": {
-            "email": sender_address,
-            "name": sender_name
-        },
-        "to": [{"email": recipient} for recipient in recipients],
-        "subject": subject,
-        "text": body,
-        "category": category
-    }
-
-    url = "https://send.api.mailtrap.io/api/send"
-    payload = json.dumps(payload)
-    headers = {
-        "Authorization": f"Bearer {SECRETS['mailtrap_token']}",
-        "Content-Type": "application/json"
-    }
-
-    response = requests.post(url, headers=headers, data=payload)
-
-    return response.text
-
-def log(message):
-    print(message)
-    debug(message)
+log = print
